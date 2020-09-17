4eed6ef754077bf34a56b7d51a6e126c68da25f1
Carlos Eduardo <carlosedu.waldorf@gmail.com>
2020-09-16 22:30:36 -0300

    add implements in class

--- src/Model/Sorts/SelectionSort/Selection_sort.java
+++ src/Model/Sorts/SelectionSort/Selection_sort.java
@@ -1,6 +1,6 @@
 package Sorts.Selection_sort;
 
-public class Selection_sort {
+public class Selection_sort implements Sort {
 
     /**
      * @param arr - Array of int values to be sorted
@@ -8,12 +8,11 @@ public class Selection_sort {
      * 
      */
     
-    public int sort (int[] arr){ // Complexity O(N^2)
+    public int sort (int[] arr) { // Complexity O(N^2)
         for (int i = 0; i < arr.length; i++) {
             int minor = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minor]) minor = j;
-            }
             change(arr, i, minor);
         }
         return arr;
